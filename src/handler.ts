3dd67ee19c5bd242bb160ff146378ad24e8142cf
Greg Brimble <developer@gregbrimble.com>
2019-10-06 20:08:51 +0100

    WIP Caching Datastore

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,33 @@
+import { Entity, Column, getRepository } from '@kv-orm/core'
+import { CloudflareWorkersKVDatastore } from '@kv-orm/cf-workers'
+
+const datastore = new CloudflareWorkersKVDatastore(myKVNamespace)
+
+@Entity({ datastore, key: 'XClass' })
+class X {
+  @Column({ key: 'myProperty' })
+  public myProp = 'greg'
+
+  @Column({ key: 'identifier', isPrimary: true })
+  public id: number
+
+  constructor(id: number) {
+    this.id = id
+  }
+}
+
+const XRepo = getRepository(X)
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const instance = new X(12345)
+  instance.myProp = 'tim'
+
+  return new Response(
+    `request method: ${
+      request.method
+    } & ${await instance.myProp} & ${await XRepo.save(
+      instance,
+    )} & ${await XRepo.save(instance)} & ${await (await XRepo.load(12345))
+      .myProp}`,
+  )
 }
